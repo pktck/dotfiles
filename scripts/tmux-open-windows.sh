91c92b746c3f5601e61f3754de3e97b80d894096
Paul Kastner <misc@paulkastner.com>
2012-09-11 14:31:21 -0700

    opening windows instead of just naming them

---
+++ scripts/tmux-open-windows.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tmux new-window -t 0 -n django
+tmux new-window -t 1 -n py
+tmux new-window -t 2 -n templates
+tmux new-window -t 3 -n js
+tmux new-window -t 4 -n handlebars
+tmux new-window -t 5 -n ipython
+tmux new-window -t 6 -n git
+tmux new-window -t 7 -n misc
+tmux new-window -t 8 -n misc
+tmux new-window -t 9 -n misc
