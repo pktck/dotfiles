413c290323a2ceaf3deed8161b0ccb47dc433b7a
Paul Kastner <misc@paulkastner.com>
2012-10-31 15:14:53 -0700

    changed window opening format

--- scripts/tmux-open-windows.sh
+++ scripts/tmux-open-windows.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-tmux new-window -t 0 -n django
-tmux new-window -t 1 -n py
-tmux new-window -t 2 -n templates
-tmux new-window -t 3 -n js
-tmux new-window -t 4 -n handlebars
-tmux new-window -t 5 -n ipython
-tmux new-window -t 6 -n git
-tmux new-window -t 7 -n misc
-tmux new-window -t 8 -n misc
-tmux new-window -t 9 -n misc
+tmux new-window -k -n django     -t 0
+tmux new-window -k -n py         -t 1
+tmux new-window -k -n templates  -t 2
+tmux new-window -k -n js         -t 3
+tmux new-window -k -n handlebars -t 4
+tmux new-window -k -n ipython    -t 5
+tmux new-window -k -n git        -t 6
+tmux new-window -k -n misc       -t 7
+tmux new-window -k -n misc       -t 8
+tmux new-window -k -n misc       -t 9
