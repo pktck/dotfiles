5abd4ddf430175bdc8f9e45fd0fe4999067fd9ce
Paul Kastner <misc@paulkastner.com>
2012-10-31 15:25:42 -0700

    took out window kill

--- scripts/tmux-open-windows.sh
+++ scripts/tmux-open-windows.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-tmux new-window -k -n django     -t 0
-tmux new-window -k -n py         -t 1
-tmux new-window -k -n templates  -t 2
-tmux new-window -k -n js         -t 3
-tmux new-window -k -n handlebars -t 4
-tmux new-window -k -n ipython    -t 5
-tmux new-window -k -n git        -t 6
-tmux new-window -k -n misc       -t 7
-tmux new-window -k -n misc       -t 8
-tmux new-window -k -n misc       -t 9
+tmux new-window -n django     -t 0
+tmux new-window -n py         -t 1
+tmux new-window -n templates  -t 2
+tmux new-window -n js         -t 3
+tmux new-window -n handlebars -t 4
+tmux new-window -n ipython    -t 5
+tmux new-window -n git        -t 6
+tmux new-window -n misc       -t 7
+tmux new-window -n misc       -t 8
+tmux new-window -n misc       -t 9
